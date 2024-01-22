25c7b67ce5dc8c294dca185872880b71d96c36cd
sasn14053423423525 <sasn1405@gmail.com>
2024-01-22 16:23:06 +0900

    chapter 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+import { Component } from "react"
+import "./App.css"
+export default class App extends Component{
+  render(){
+    return (
+      <main className="main-container">
+        <div className="sub-container">
+          <h1>장바구니</h1>
           
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <div style={{width:'100%', backgroundColor:"white", padding:'1rem'}}>
+            {/* Expense Form */}Expense Form
+          </div>
 
-export default App;
+          <div style={{width:'100%', backgroundColor:"white", padding:'1rem'}}>
+            {/* Expense List */}Expense List
+          </div>
+
+          <div style={{display:"flex", justifyContent:'start', marginTop:'1rem'}}>
+            <p style={{fontSize:"2rem"}}>
+              총 합계:
+            </p>
+          </div>
+        </div>
+      </main>
+    )
+  }
+}
